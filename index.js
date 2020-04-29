d0cebd50280e1c1af669637cc08ee94755b418c3
Rohit <rohit5822540@gmail.com>
2020-04-29 20:05:31 +0530

    DOM manipulation on Input from user example

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function buttonclick(){
-    var str=document.getElementById("heading").innerHTML="Clicked";
+    var str1=document.getElementById("text1").value;
+    var str2=document.getElementById("text2").value;
+    if(str1==str2){
+        alert("yes");
+    }
+    else{
+        alert("no");
+    }
 }
